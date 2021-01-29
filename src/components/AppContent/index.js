564109c89884d04cce12580f5bd60b7e59eb7843
Anush Chakhoyan <anush.chakhoyan@develandoo.am>
2021-01-30 00:44:06 +0400

    Added separated components

---
+++ src/components/AppContent/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AppContent = ({description}) => (
+    <section className="container">
+        <h2>Content</h2>
+        <p>{description}</p>
+    </section>
+);
+
+export default AppContent;
