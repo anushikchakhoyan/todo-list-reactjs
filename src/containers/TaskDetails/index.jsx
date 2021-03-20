d70f5109b4fd587b92a7c554cd1ce0d789c4b0ba
Anush Chakhoyan <anush.chakhoyan@develandoo.am>
2021-03-21 02:18:54 +0400

    added edit button functionality for single task and loading eith their functionality

--- src/containers/TaskDetails/index.jsx
+++ src/containers/TaskDetails/index.jsx
@@ -1,18 +1,26 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import {Breadcrumb, Button, Card, Col, Container, Row} from "react-bootstrap";
 
+import HttpStatusCode from "../../constants/HttpStatusCode";
 import ConfirmModal from "../../components/ConfirmModal";
+import AppLoading from "../../components/AppLoading";
 import formatDate from "../../helpers/date.helper";
+import AddEditTaskModal from "../../components/ToDo/AddEditTaskModal";
 
 class TaskDetailsContainer extends Component {
     state = {
         task: {},
+        isLoading: false,
+        isEditModalVisible: false,
         isConfirmModalVisible: false
     }
 
     componentDidMount() {
         const {id} = this.props.match.params;
+        const {history} = this.props;
+
+        this.setState({isLoading: true});
         fetch(`http://localhost:3001/task/${id}`)
             .then(res => res.json())
             .then(data => {
@@ -24,12 +32,18 @@ class TaskDetailsContainer extends Component {
                 });
             })
             .catch(error => {
-                console.error("Get Single Task Request Error", error);
-            });
+                if (error.status === HttpStatusCode.SOMETHING_WENT_WRONG) {
+                    return history.push('/404');
+                }
+            })
+            .finally(() => {
+                this.setState({isLoading: false})
+            })
     }
 
     handleRemoveSingleTask = () => {
         const {id} = this.props.match.params;
+        this.setState({isLoading: true});
         fetch(`http://localhost:3001/task/${id}`, {
             method: "DELETE"
         })
@@ -38,38 +52,56 @@ class TaskDetailsContainer extends Component {
                 if (data.error) {
                     throw data.error;
                 }
+            })
+            .catch(error => {
+                console.error("Delete single task Request Error", error);
+            })
+            .finally(() => {
+                this.setState({isLoading: false})
+            })
+    }
+
+    handleEditTask = (formData) => {
+        fetch("http://localhost:3001/task/" + formData._id, {
+            method: "PUT",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.error)
+                    throw data.error
                 this.setState({
                     task: data
                 });
             })
             .catch(error => {
-                console.error("Delete single task Request Error", error);
+                console.log("Single Task Page,Edit Task Error", error);
             });
     }
 
     handleModalClose = () => {
-        const { history } = this.props;
+        const {history} = this.props;
         this.setState({isConfirmModalVisible: !this.state.isConfirmModalVisible})
         history.push("/");
     }
 
     render() {
-        const {task, isConfirmModalVisible} = this.state;
-        const {Item} = Breadcrumb;
+        const {task, isLoading, isEditModalVisible, isConfirmModalVisible} = this.state;
 
-        if (!task) {
-            return <div>
-                <span>Loading...</span>
-            </div>
+        if (isLoading) {
+            return <AppLoading/>
         }
 
         return (
             <Container>
                 <Breadcrumb>
-                    <Item>
-                        <Fragment><Link to="/">Tasks</Link></Fragment>
-                    </Item>
-                    <Item active>{task._id}</Item>
+                    <li className="breadcrumb-item">
+                        <Link to="/">Tasks</Link>
+                    </li>
+                    <li className="breadcrumb-item active">{task._id}</li>
                 </Breadcrumb>
                 <Row>
                     <Col lg={10}>
@@ -77,7 +109,7 @@ class TaskDetailsContainer extends Component {
                             <Card.Header>{task.title}</Card.Header>
                             <Card.Body>
                                 <div className="py-4">
-                                    <p className="font-weight-bold">Desciption</p>
+                                    <p className="font-weight-bold">Description</p>
                                     <p>{task.description}</p>
                                 </div>
                                 <blockquote className="blockquote mb-0">
@@ -96,12 +128,19 @@ class TaskDetailsContainer extends Component {
                         </Card>
                     </Col>
                     <Col lg={2} className="d-flex flex-column">
-                        <Button variant="outline-secondary" className="mb-2">Edit</Button>
-                        <Button variant="outline-secondary" className="mb-2" onClick={() => {
-                            this.setState({
-                                isConfirmModalVisible: !this.state.isConfirmModalVisible
-                            })
-                        }}>Delete</Button>
+                        <Button variant="outline-secondary" className="mb-2"
+                                onClick={() => {
+                                    this.setState({
+                                        isEditModalVisible: !this.state.isEditModalVisible
+                                    })
+                                }}
+                        >Edit</Button>
+                        <Button variant="outline-secondary" className="mb-2"
+                                onClick={() => {
+                                    this.setState({
+                                        isConfirmModalVisible: !this.state.isConfirmModalVisible
+                                    })
+                                }}>Delete</Button>
                     </Col>
                 </Row>
                 {isConfirmModalVisible && <ConfirmModal
@@ -109,6 +148,15 @@ class TaskDetailsContainer extends Component {
                     onClose={this.handleModalClose}
                     onSubmit={this.handleRemoveSingleTask}
                     message={`Would you like to remove task?`}
+                />}
+                {isEditModalVisible && <AddEditTaskModal
+                    isShow={true}
+                    modalTitle="Edit Task"
+                    editableTask={task}
+                    onSubmit={this.handleEditTask}
+                    onHide={() => {
+                        this.setState({isEditModalVisible: null})
+                    }}
                 />}
             </Container>
         )
